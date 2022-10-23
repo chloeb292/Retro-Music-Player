6eeb7ab8877d713f339f2fb62f06cc9e2fbf737a
chloeb292 <66789673+chloeb292@users.noreply.github.com>
2022-10-23 04:33:17 -0400

    Initial Commit

---
+++ Retro Music Player/Retro_Music_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Retro_Music_PlayerApp.swift
+//  Retro Music Player
+//
+//  Created by Chloe Berry on 10/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Retro_Music_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
